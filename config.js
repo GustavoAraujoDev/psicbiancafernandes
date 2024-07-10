b9a5ba7cb6a452368e398ccb65717a6688295b24
gustavoaraujodev <gustavobarrosaraujo10@gmail.com>
2024-07-10 10:45:52 -0300

    versao 1.0

---
+++ config.js
@@ -0,0 +1,31 @@
+document.addEventListener('scroll', function() {
+    const section2 = document.getElementById('section2');
+    const sectionPosition = section2.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.5;
+
+    if (sectionPosition < screenPosition) {
+        section2.style.opacity = '1';
+        section2.style.transform = 'translateY(0)';
+    }
+});
+document.addEventListener('scroll', function() {
+    const section4 = document.getElementById('section4');
+    const sectionPosition = section4.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.5;
+
+    if (sectionPosition < screenPosition) {
+        section4.style.opacity = '1';
+        section4.style.transform = 'translateY(0)';
+    }
+});
+
+document.addEventListener('scroll', function() {
+    const section6 = document.getElementById('section6');
+    const sectionPosition = section6.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.5;
+
+    if (sectionPosition < screenPosition) {
+        section6.style.opacity = '1';
+        section6.style.transform = 'translateY(0)';
+    }
+});
